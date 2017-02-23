073cb864f501832c44588003e07e84b4ae8589a4
Jeff <crostonj@gmail.com>
2017-02-23 16:24:46 -0700

    Added servername path

--- src/serverinfoRouter.js
+++ src/serverinfoRouter.js
@@ -10,17 +10,7 @@ module.exports = router = function () {
     });
 
     infoRouter.get('/servername', function (req, res) {
-        setInterval(function () {
-            res.render('index', {
-                host: hostinfo
-            });
-        }, 5);
-    });
-
-    infoRouter.get('/ip', function (req, res) {
-
-        var network = os.networkInterfaces();
-        res.send(network.eth0[0].address);
+            res.send(hostinfo.hostname);
     });
 
     infoRouter.get('/host', function (req, res) {
